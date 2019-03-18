54394843acb8bc8dcf7f2606f58863eb0af311e0
almyk <hansentommy92@gmail.com>
2019-03-18 13:52:17 +0900

    Initial Commit

---
+++ git.xcode/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  git.xcode
+//
+//  Created by Tommy Hansen on 18/03/2019.
+//  Copyright © 2019 Tommy Hansen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
